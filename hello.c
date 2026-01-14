03163b8a576e36224ef02a7f79850e735e7a08e3
realdanvanth <realdanvanth@gmail.com>
2026-01-14 20:32:22 +0530

    init2

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <linux/init.h>
 #include <linux/module.h>
-static int init() { printk("hello world"); }
-static void exit() { printk("exiting the program"); }
+static int __init init(void) {
+  printk("hello world");
+  return 0;
+}
+static void __exit exit(void) { printk("exiting the program"); }
 module_init(init);
 module_exit(exit);
+MODULE_LICENSE("GPL");
