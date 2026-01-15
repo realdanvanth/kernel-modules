628f0542b9839d720695a82a8d8e045d47a2ddcb
realdanvanth <realdanvanth@gmail.com>
2026-01-16 00:06:26 +0530

    read and write

--- src/hello.c
+++ src/hello.c
@@ -16,18 +16,39 @@ static int release(struct inode *, struct file *) {
 }
 static ssize_t read(struct file *filep, char __user *user, size_t l,
                     loff_t *off) {
-  int not_copied, delta,
-      to_copy = (len + *off) < size(text) ? len : (sizeof(text) - *off);
-  return 0;
+  size_t index = l + *off;
+  if (*off >= sizeof(data)) {
+    return 0;
+  } else if (index >= sizeof(data)) {
+    l = sizeof(data) - *off;
+  }
+  if (copy_to_user(user, &data[*off], l))
+    return 0;
+  printk("read\n");
+  *off += l;
+  return l;
 }
-ssize_t write(struct file *, const char __user *, size_t, loff_t *) {
-
-  return 0;
+ssize_t write(struct file *filep, const char __user *user, size_t l,
+              loff_t *off) {
+  size_t index = l + *off;
+  if (*off >= sizeof(data)) {
+    return 0;
+  } else if (index >= sizeof(data)) {
+    l = sizeof(data) - *off;
+  }
+  if (copy_from_user(&data[*off], user, l)) {
+    return 0;
+  }
+  *off = *off + l;
+  printk("written\n");
+  return l;
 }
-static struct file_operations fops = {.open = open, .release = release};
+static struct file_operations fops = {
+    .open = open, .release = release, .read = read, .write = write};
 static int __init init(void) {
   printk("module initialized\n");
-  register_chrdev(major, DEVICE, &fops);
+  major = register_chrdev(0, DEVICE, &fops);
+  printk("major: %d\n", major);
   return 0;
 }
 static void __exit exit(void) {
